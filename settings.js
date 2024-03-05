cf36904eb5843bdea0e2fef7e219d6e455c5f934
Guakocius <engelhardt.alexander@mein.gmx>
2024-03-05 11:32:09 +0100

    Added style to body and the settings

--- settings.js
+++ settings.js
@@ -1,16 +1,3 @@
 document.addEventListener("DOMContentLoaded", function () {
-    let darkMode
-    document.getElementById("darkMode").checked = localStorage.getItem("darkTheme") === "true"
 
-    document.getElementById("settingsForm").addEventListener("submit", function(e) {
-        e.preventDefault()
-         darkMode = document.getElementById("darkMode").checked
-        localStorage.setItem("darkTheme", darkMode)
-        applyTheme(darkMode)
-    })
-
-    function applyTheme(darkMode) {
-        document.body.classList.toggle("dark-mode", darkMode
-        )
-    }
 })
